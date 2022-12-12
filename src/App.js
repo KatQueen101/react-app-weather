688835a5ae2c52802de69253aa185f3746c31d53
KatQueen101 <brenae1999@hotmail.com>
2022-12-12 17:26:04 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Alexandria Goss and open-sourced on{" "}
-        <a
-          href="https://github.com/KatQueen101/react-app-weather"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Alexandria Goss and open-sourced on{" "}
+          <a
+            href="https://github.com/KatQueen101/react-app-weather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
