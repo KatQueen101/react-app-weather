28cff4d3f466e6e09deb1dfabc67ade350ff85ad
KatQueen101 <brenae1999@hotmail.com>
2022-12-12 17:37:40 -0500

    Adding files and importing links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Alexandria Goss and open-sourced on{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return <div className="Weather">Hello from Weather</div>;
+}
