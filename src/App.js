bde71708eeff7807a1dac2ef96e02f756b108c8f
KatQueen101 <brenae1999@hotmail.com>
2022-12-12 15:13:09 -0500

    Added github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Alexandria Goss and open-sourced on{" "}
+        <a
+          href="https://github.com/KatQueen101/react-app-weather"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
