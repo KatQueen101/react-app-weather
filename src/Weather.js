c75d01ece80a8e3324ff917e6b24c21cc0aad6c6
KatQueen101 <brenae1999@hotmail.com>
2022-12-15 12:23:48 -0500

    Change clearfix to d-flex

--- src/Weather.js
+++ src/Weather.js
@@ -47,7 +47,7 @@ export default function Weather(props) {
         </ul>
         <div className="row mt-3">
           <div className="col-6">
-            <div className="clearfix">
+            <div className="d-flex">
               <img
                 src={weatherData.iconUrl}
                 alt={weatherData.description}
