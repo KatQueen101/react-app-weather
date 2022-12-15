01667c0be612528770da5a2f6fb7268352fef71e
KatQueen101 <brenae1999@hotmail.com>
2022-12-15 16:23:34 -0500

    Weather Icons

---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">℃</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
